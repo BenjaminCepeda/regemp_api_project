d71b3df018875d791ed5fe27830d1c7f4a45d52e
Benjamín Cepeda <bcepeda_98@yahoo.com>
2022-05-13 00:36:05 -0500

    Añadir requerimientos Heroku deploy

--- regemp_api/urls.py
+++ regemp_api/urls.py
@@ -4,11 +4,11 @@ from regemp_api import views
 urlpatterns = [
     path('departamento/<id>/', views.DepartamentoDetail.as_view()),
     path('departamento/', views.DepartamentoList.as_view()),
-    path('empleado/<id>', views.EmpleadoDetail.as_view()),
+    path('empleado/<id/', views.EmpleadoDetail.as_view()),
     path('empleado/', views.EmpleadoList.as_view()),
-    path('perfil/<id>', views.PerfilDetail.as_view()),
+    path('perfil/<id>/', views.PerfilDetail.as_view()),
     path('perfil/', views.PerfilList.as_view()),
-    path('usuario/<id>', views.UsuarioDetail.as_view()),
+    path('usuario/<id>/', views.UsuarioDetail.as_view()),
     path('usuario/', views.UsuarioList.as_view()),
 
 ]
